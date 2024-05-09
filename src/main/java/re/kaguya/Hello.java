3a732bb02f4c8f00ebed009aaf69b2f31ece345d
Kaguya <rider0@163.com>
2024-05-09 09:51:54 +0800

    第三次提交

--- src/main/java/re/kaguya/Hello.java
+++ src/main/java/re/kaguya/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("你是内个内个内内内....");
         System.out.println("白巧:你好啊我的内个");
+        System.out.println("黑巧:? m3,you w t a say? a y fk k l m ?");
 
     }
 }
