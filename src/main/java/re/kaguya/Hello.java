cd03401619f5d9d9d06d4eb464e19e1a892a2232
Kaguya <rider0@163.com>
2024-05-09 09:48:32 +0800

    第二次提交

--- src/main/java/re/kaguya/Hello.java
+++ src/main/java/re/kaguya/Hello.java
@@ -3,6 +3,7 @@ package re.kaguya;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("你是内个内个内内内....");
+        System.out.println("白巧:你好啊我的内个");
 
     }
 }
